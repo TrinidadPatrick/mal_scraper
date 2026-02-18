7ff48a2873927c76c96b8748c7d75c2b17105879
TrinidadPatrick <trinidadpatrick019@gmail.com>
2026-02-18 21:27:15 +0800

    feat: Expand anime scraping to include movies and add progress output

--- scraper.py
+++ scraper.py
@@ -5,7 +5,6 @@ import json
 from pathlib import Path
 
 def save_to_csv(data, filename):
-    print(data[0].keys())
     with open(filename, 'w', newline='', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
@@ -18,13 +17,16 @@ def run():
             ('output_json/top_animes/top_airing_animes.json', 'airing'),
             ('output_json/top_animes/top_upcoming_animes.json', 'upcoming'),
             ('output_json/top_animes/top_tv_animes.json', 'tv'),
+            ('output_json/top_animes/top_movie_animes.json', 'movie'),
         ]
         
         for filename, type in tasks:
+            print(f'Scraping {type} animes...')
             data = scrape_top_animes(page, type)
             file_path = Path(filename)
             file_path.parent.mkdir(parents=True, exist_ok=True)
             save_to_csv(data, file_path)
+            print(f'Done scraped {type} animes...')
         
 def main():
     run()
