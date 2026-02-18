d2c070bb3583991d87d1b6e8fca09a668f2ec738
John Patrick Trinidad <Ptrinidad765@gmail.com>
2026-02-18 12:01:16 +0800

    Added a function to fetch top animes with pagination

--- scraper.py
+++ scraper.py
@@ -1,31 +1,45 @@
 from playwright.sync_api import sync_playwright
 import csv
 
+def get_top_animes(page):
+    top_anime_lists = page.locator(".ranking-list").all()
+
+    top_anime_titles = []
+
+    for index, book in enumerate(top_anime_lists):
+        title_container = book.locator('.title')
+        title = title_container.locator('.anime_ranking_h3 a').inner_text()
+        top_anime_titles.append({'title': title})
+    
+    return top_anime_titles
+
 def run():
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=True)    
+        browser = playwright.chromium.launch(headless=False)    
         page = browser.new_page()
 
         print("Navigating to the MAL...")
+        
         page.goto("https://myanimelist.net/topanime.php")
+        page.locator('#accept-btn').click()
+        results = []
+        
+        for i in range(20):
+            result = get_top_animes(page)
+            results.extend(result)
+            next_btn_selector = 'a.link-blue-box.next'
+            page.wait_for_selector(next_btn_selector)
+            with page.expect_navigation():
+                page.locator(next_btn_selector).first.click()
+                
+        with open('top_animes.csv', 'w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=['title'])
 
-        top_anime_lists = page.locator(".ranking-list").all()
+            writer.writeheader()
 
-        top_anime_titles = []
+            writer.writerows(test)
 
-        for index, book in enumerate(top_anime_lists):
-            title_container = book.locator('.title')
-            title = title_container.locator('.anime_ranking_h3 a').inner_text()
-            top_anime_titles.append({'title': title})
-        # print(top_anime_titles)
-        if(len(top_anime_titles) > 0):
-            with open('top_animes.csv', 'w', newline='') as file:
-                writer = csv.DictWriter(file, fieldnames=['title'])
-
-                writer.writeheader()
-
-                writer.writerows(top_anime_titles)
-
+        print("Data scrapped")
 
 def main():
     run()
